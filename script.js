f8a81d34ecd8d8210bd87d3b0f720d804007dddc
Ayan <57215535+ayank007@users.noreply.github.com>
2023-03-18 17:35:54 +0530

    Vanilla starter

--- script.js
+++ script.js
@@ -1,20 +1,60 @@
-document.addEventListener("DOMContentLoaded", function(event){
-  
-    // Append HTML in DOM from JavaScript
-    const appendHtml = (el, str) => {
-        var div = document.createElement('div')
-        div.innerHTML = str
-        while (div.children.length > 0) {
-            el.appendChild(div.children[0])
+// Append HTML in DOM from JavaScript
+const appendHtml = (el, str) => {
+    var div = document.createElement('div')
+    div.innerHTML = str
+    while (div.children.length > 0) {
+        el.appendChild(div.children[0])
+    }
+}
+
+// form submit
+const onSubmit = (e) => {
+    e.preventDefault()
+    
+//     getting form data
+    const formData = new FormData()
+//     appending data
+    formData.append('username', 'john')
+    formData.append('email', 'john@example.com')
+//     appending files
+    const fileInput = document.querySelector('input[type="file"].outSideFile')
+    const file = fileInput.files[0]
+    formData.append('file', file)
+    
+//     XML request
+    var xhr = new XMLHttpRequest()
+    xhr.open('POST', API, true)
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText)
+        } else {
+            console.log('Error: ' + xhr.status)
         }
     }
+    xhr.send(formData)
+}
 
-    // setting option to know when a section appears in screen
-    const appearOptions = {
-        threshold: 0,
-        rootMargin: "-250px 0px -250px 0px"
-    }
+// form submit JSON
+fetch(API, {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: JSON.stringify(data),
+})
+.then(res=>res.json())
+.then((result)=>{})
 
+
+// setting option to know when a section appears in screen
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "-250px 0px -250px 0px"
+}
+
+// document on loaded
+document.addEventListener("DOMContentLoaded", function(event){
     // swiper slider
     const swiper = new Swiper(".portfolioSlider", {
         slidesPerView: 1,
@@ -43,6 +83,7 @@ document.addEventListener("DOMContentLoaded", function(event){
         }
     })
     
+    // Scroll Based Animation Starter     
     const appearOnScroll = new IntersectionObserver(function(
         entries,
         appearOnScroll
@@ -57,7 +98,6 @@ document.addEventListener("DOMContentLoaded", function(event){
             }
         })
     }, appearOptions)
-    
     appearOnScroll.observe(swiper)
   
 })
